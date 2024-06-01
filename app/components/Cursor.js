d54fff96d63f90f8fd023d9e731e28f8cd4f3025
maxim12313 <maximkim16@gmail.com>
2024-05-31 22:34:15 -0700

    cursor dissapears when off screen now

--- app/components/Cursor.js
+++ app/components/Cursor.js
@@ -5,14 +5,14 @@ import { useEffect, useState } from "react"
 export default function Cursor() {
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
+  const [showing, setShowing] = useState(false);
   
   const locationStyle = {
     top: mouseY,
     left: mouseX,
     transform: "translate(-50%,-50%)",
   };
-  
-  
+
   const cursor = (  
     <div>
       <div 
@@ -24,7 +24,6 @@ export default function Cursor() {
         className="fixed p-1 bg-white rounded-full z-20 pointer-events-none"
         style={locationStyle}
       />
-
     </div>
   );
 
@@ -32,14 +31,23 @@ export default function Cursor() {
     const mouseMove = (e) => {
       setMouseX(e.clientX);
       setMouseY(e.clientY);
+      setShowing(true);
     };
 
+    const mouseOut = (e) => {
+      setShowing(false);
+    }
+
     window.addEventListener("mousemove", mouseMove);
+    window.addEventListener("mouseout", mouseOut);
 
     return () => {
       window.removeEventListener("mousemove", mouseMove);
+      window.removeEventListener("mouseout", mouseOut);
     }
   }, []);
 
-  return cursor;
+
+
+  return showing ? cursor : "";
 }
